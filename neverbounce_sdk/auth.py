4796f51293735fd889ebce5295996ba4df1324cc
John Still <john@jmsdvl.com>
2017-07-24 12:37:29 -0500

    auth append token to url no matter the verb

--- neverbounce_sdk/auth.py
+++ neverbounce_sdk/auth.py
@@ -7,15 +7,20 @@ __all__ = ['StaticTokenAuth']
 
 
 class StaticTokenAuth(AuthBase):
-    """Uses a static token to authenticate with NeverBounce's API v4"""
+    """Uses a static token to authenticate with NeverBounce's API v4
+
+    Args:
+        token (str): A static API token for authentication
+    """
 
     def __init__(self, token):
         self.token = token
         self.token_d = dict(key=token)
 
     def __call__(self, request):
-        if request.method == 'GET':
-            request.prepare_url(request.url, self.token_d)
-        elif request.method == 'POST':
-            request.data.update(self.token_d)
+        # Apparently this will work no matter what HTTP method is used, so...
+        # this is simplest, just do this.  NOTE: at this point in time, if the
+        # request is a POST/PATCH/PUT, the body is already encoded and there is
+        # no non-ugly way to just attach the token to it
+        request.prepare_url(request.url, self.token_d)
         return request
