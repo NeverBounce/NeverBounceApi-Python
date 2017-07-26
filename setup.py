354916b49146004cde3c491ce5c5b4b99d44500c
Mike Mollick <mollick2@gmail.com>
2017-07-26 12:09:34 -0400

    Fixing imports in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
 here = path.abspath(path.dirname(__file__))
 
