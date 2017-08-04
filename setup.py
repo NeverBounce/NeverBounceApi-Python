264e0f7c27305e5b0b214f80d5ec046b63057ee9
Mike Mollick <mollick2@gmail.com>
2017-08-04 18:33:04 -0400

    bump to 1.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
-from neverbounce_sdk import __version__
 
 here = path.abspath(path.dirname(__file__))
 
@@ -12,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='neverbounce-sdk',
-    version=__version__,
+    version='1.0.5',
     description="Official Python SDK for the NeverBounce API",
     long_description=long_description,
     author="NeverBounce Team",
