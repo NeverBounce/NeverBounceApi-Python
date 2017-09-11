9379b6fa7ab3b537bb60c8d78dc55c5b8153be5f
Mike Mollick <mollick2@gmail.com>
2017-09-11 17:28:07 -0400

    Bumping version and adding better version resolution in setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,29 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+from re import search, M
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    # see here: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    return open(path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
     name='neverbounce-sdk',
-    version='4.1.0',
+    version=find_version('neverbounce_sdk/__init__.py'),
     description="Official Python SDK for the NeverBounce API",
     long_description=long_description,
     author="NeverBounce Team",
