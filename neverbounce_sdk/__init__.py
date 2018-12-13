18d13947660a59a40a9dd8ce39731c63a1e30a23
Mike Mollick <mollick2@gmail.com>
2018-12-13 13:07:53 -0500

    Bumping version number

--- neverbounce_sdk/__init__.py
+++ neverbounce_sdk/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "NeverBounce Team"
 __email__ = 'support@neverbounce.com'
-__version__ = '4.2.5'
+__version__ = '4.2.6'
 
 from .auth import *         # noqa: F403
 from .exceptions import *   # noqa: F403
